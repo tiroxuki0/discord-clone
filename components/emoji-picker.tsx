45601c02f7a653b9091f69a8b7136b48cc9fd31a
Minh Huy <citythree.11798@gmail.com>
2025-06-11 16:19:57 +0700

    Update emoji-mart dependencies and refactor EmojiPicker component

--- components/emoji-picker.tsx
+++ components/emoji-picker.tsx
@@ -1,42 +1,40 @@
-"use client";
+"use client"
 
-import { Smile } from "lucide-react";
-import Picker from "@emoji-mart/react";
-import data from "@emoji-mart/data";
-import { useTheme } from "next-themes";
-
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover"
+import { Smile } from "lucide-react"
+import { useTheme } from "next-themes"
+import { useEffect, useState } from "react"
 
 interface EmojiPickerProps {
-  onChange: (value: string) => void;
+  onChange: (value: string) => void
 }
 
-export const EmojiPicker = ({
-  onChange,
-}: EmojiPickerProps) => {
-  const { resolvedTheme } = useTheme();
+export const EmojiPicker = ({ onChange }: EmojiPickerProps) => {
+  const { resolvedTheme } = useTheme()
+  const [isMounted, setIsMounted] = useState(false)
+  const [Picker, setPicker] = useState<any>(null)
+
+  useEffect(() => {
+    setIsMounted(true)
+
+    const loadEmoji = async () => {
+      const data = await import("@emoji-mart/data")
+      const picker = (await import("@emoji-mart/react")).default
+      setPicker(() => picker)
+    }
+
+    loadEmoji()
+  }, [])
+
+  if (!isMounted) return null
 
   return (
     <Popover>
       <PopoverTrigger>
-        <Smile
-          className="text-zinc-500 dark:text-zinc-400 hover:text-zinc-600 dark:hover:text-zinc-300 transition"
-        />
+        <Smile className="text-zinc-500 dark:text-zinc-400 hover:text-zinc-600 dark:hover:text-zinc-300 transition" />
       </PopoverTrigger>
-      <PopoverContent 
-        side="right" 
-        sideOffset={40}
-        className="bg-transparent border-none shadow-none drop-shadow-none mb-16"
-      >
-        <Picker
-          theme={resolvedTheme}
-          data={data}
-          onEmojiSelect={(emoji: any) => onChange(emoji.native)}
-        />
+      <PopoverContent side="right" sideOffset={40} className="bg-transparent border-none shadow-none drop-shadow-none mb-16">
+        {Picker && <Picker data={import("@emoji-mart/data")} theme={resolvedTheme || "dark"} onEmojiSelect={(emoji: any) => onChange(emoji.native)} />}
       </PopoverContent>
     </Popover>
   )
